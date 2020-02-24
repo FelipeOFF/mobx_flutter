9d9a948408b6c6ab4d16201bd7190e40b1043e31
FelipeOFF <felipe-not@hotmail.com>
2020-02-24 13:18:10 -0300

    add mobx to project

--- lib/home.dart
+++ lib/home.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+import 'controller.dart';
 
 class MyHomePage extends StatelessWidget {
-  
-  int counter = 0;
+  final controller = Controller();
 
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: Scaffold(
-         appBar: AppBar(
-           title: Text("MobX"),
-         ),
-         body: Center(
-           child: Column(
-             mainAxisAlignment: MainAxisAlignment.center,
-             children: <Widget>[
-               Text(
-                 '$counter',
-                 style: Theme.of(context).textTheme.display1,
-               )
-             ],
-           ),
-         ),
-       ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("MobX"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Observer(
+                builder: (BuildContext context) {
+                  return Text(
+                    '${controller.counter.value}',
+                    style: Theme.of(context).textTheme.display1,
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            controller.increment();
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
     );
   }
 }
